70ca944114938bb59e1e1acdb69725200e2af251
gustavocaruazu <gustavocaruazu@gmail.com>
2023-04-04 19:53:19 -0300

    fix(main lopp): fix main loop error

--- catavento.py
+++ catavento.py
@@ -1,3 +1,5 @@
+# MEDIDA DE EFICIENCIA DA DISCIPLINA DE COMPUTAÇÃO VISUAL
+
 import cv2
 import numpy as np
 from collections import namedtuple
@@ -20,6 +22,8 @@ BLUE =  (255,  0 ,  0 )
 GREEN = ( 0 , 255,  0 )
 RED =   ( 0 ,  0 , 255)
 
+scale = 5
+
 canvas = np.ones(WINDOW_SIZE, dtype="uint8") * 40
 
 def draw_obj_1(x:int, y:int, scale:int):
@@ -35,7 +39,7 @@ def draw_obj_2(x:int,y:int , scale:int):
 def draw_obj_3(x:int,y:int , scale:int):
     point_a = [x + 12*scale,y - 22*scale]
     point_b = [x - 26*scale,y - 22*scale]
-    triangle = np.array([point_a,point_b,[320, 280]], np.int32)
+    triangle = np.array([point_a, point_b,[320, 280]], np.int32)
     cv2.fillPoly(canvas,[triangle],BLUE)
 
     point_a2 =  [x - 13*scale, y - 18*scale]
@@ -49,20 +53,29 @@ def draw_obj_4(x:int, y:int , scale:int):
 def draw_obj_5(x:int, y:int , scale:int):
     pass
     
-canvas = np.ones(WINDOW_SIZE, dtype="uint8") * 40
-# MEDIDA DE EFICIENCIA DA DISCIPLINA DE COMPUTAÇÃO VISUAL
-
-while True:
-    user_input = input("para encerrar aperte q")
-
-    draw_obj_1(obj_1.x, obj_1.y, scale=5)
-    draw_obj_2(obj_2.x, obj_2.y, scale=5)
-    draw_obj_3(obj_3.x, obj_3.y, scale=5)
-    draw_obj_4(obj_4.x, obj_4.y, scale=5)
-    draw_obj_5(obj_5.x, obj_5.y, scale=5)
-
+def draw_canvas():
+    draw_obj_1(obj_1.x, obj_1.y, scale=scale)
+    draw_obj_2(obj_2.x, obj_2.y, scale=scale)
+    draw_obj_3(obj_3.x, obj_3.y, scale=scale)
+    draw_obj_4(obj_4.x, obj_4.y, scale=scale)
+    draw_obj_5(obj_5.x, obj_5.y, scale=scale)
     cv2.imshow("Catavento", canvas)
+
+draw_canvas()
+while True:
+    key = cv2.waitKey(1000)
+
+    # it waits till we press a key
+    key = cv2.waitKey(0)
     
-    print("você digitou:", user_input)
-    if user_input == "quit":
+    # if we press esc
+    if key == 27:
+        print('esc is pressed closing all windows')
+        cv2.destroyAllWindows()
         break
+    
+    draw_canvas()
+
+if cv2.getWindowProperty('sunset', cv2.WND_PROP_VISIBLE) < 1:
+    print("ALL WINDOWS ARE CLOSED")
+cv2.waitKey(1)
