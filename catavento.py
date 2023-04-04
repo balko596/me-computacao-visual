26864bb2d52f6d7bcbbe7d7aa9cd946c68823a68
gustavocaruazu <gustavocaruazu@gmail.com>
2023-04-04 19:00:54 -0300

    comentarios

--- catavento.py
+++ catavento.py
@@ -1,7 +1,12 @@
+# MEDIDA DE EFICIENCIA DA DISCIPLINA DE COMPUTAÇÃO VISUAL
+
 while True:
     user_input = input("para encerrar aperte q")
 
-        
+    # escala um objeto
+    # rotaciona um objeto
+    # mover um objeto
+
     print("você digitou:", user_input)
     if user_input == "quit":
         break
